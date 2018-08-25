612c945e411af3c1d8eb1603c12c55f34a7394c5
Nick Sosinski <Nick.j.sosinski@hotmail.com>
2018-08-25 14:28:57 -0500

    Adding some functionality

---
+++ Mutator/Mutator.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Mutator
+{
+    public static class Mutator
+    {
+        public static IEnumerable<string> Mutate(string filePath, Mutatations original, Mutatations mutated)
+        {
+            SyntaxNode root = CSharpSyntaxTree.ParseText(File.ReadAllText(filePath)).GetRoot();
+            return Rewriter.GetAllCombinations(root, original, mutated);
+        }
+
+        public enum Mutatations
+        {
+            Add,
+            Subract,
+            Multiply,
+            Divide
+        }
+
+        public static IEnumerable<Assembly> MutateSingleFile(string filePath, Mutatations original, Mutatations mutated)
+        {
+            CSharpCodeProvider csc = new CSharpCodeProvider();
+            CompilerParameters parameters = new CompilerParameters();
+            foreach (var item in Mutate(filePath, original, mutated))
+            {
+                yield return csc.CompileAssemblyFromSource(parameters, item).CompiledAssembly;
+            }
+        }
+
+        private static class SyntaxNodeTools
+        {
+            public static SyntaxNode CloneNode(SyntaxNode node)
+            {
+                return CSharpSyntaxTree.ParseText(node.ToString()).GetRoot();
+            }
+        }
+
+        private class Rewriter : CSharpSyntaxRewriter
+        {
+            private readonly int requestCount;
+            private int count = 0;
+            private bool changeMade = false;
+            private readonly Mutatations Original;
+            private readonly Mutatations MutateTo;
+            public static IEnumerable<string> GetAllCombinations(SyntaxNode node, Mutatations original, Mutatations mutateTo)
+            {
+                int i = 0;
+                Rewriter rewriter = new Rewriter(i, original, mutateTo);
+                yield return rewriter.Visit(SyntaxNodeTools.CloneNode(node)).ToString();
+                while (rewriter.changeMade)
+                {
+                    rewriter = new Rewriter(++i, original, mutateTo);
+                    yield return rewriter.Visit(SyntaxNodeTools.CloneNode(node)).ToString();
+                }
+            }
+            private Rewriter(int inRequestCount, Mutatations original, Mutatations mutateTo)
+            {
+                Original = original;
+                MutateTo = mutateTo;
+                requestCount = inRequestCount;
+            }
+
+            private ExpressionSyntax ConvertTo(BinaryExpressionSyntax original)
+            {
+                char operatorChar = ' ';
+                switch (MutateTo)
+                {
+                    case Mutatations.Add:
+                        operatorChar = '+';
+                        break;
+                    case Mutatations.Subract:
+                        operatorChar = '-';
+                        break;
+                    case Mutatations.Multiply:
+                        operatorChar = '*';
+                        break;
+                    case Mutatations.Divide:
+                        operatorChar = '/';
+                        break;
+                    default:
+                        throw new Exception();
+                }
+                return SyntaxFactory.ParseExpression($"{original.Left} {operatorChar} {original.Right}");
+            }
+
+            public override SyntaxNode VisitBinaryExpression(BinaryExpressionSyntax node)
+            {
+                if ((Original == Mutatations.Add && node.IsKind(SyntaxKind.AddExpression)
+                    || Original == Mutatations.Subract && node.IsKind(SyntaxKind.SubtractExpression)
+                    || Original == Mutatations.Multiply && node.IsKind(SyntaxKind.MultiplyExpression)
+                    || Original == Mutatations.Divide && node.IsKind(SyntaxKind.DivideExpression))
+                    && count++ == requestCount)
+                {
+                    changeMade = true;
+                    return base.Visit(ConvertTo(node));
+                }
+                return base.VisitBinaryExpression(node);
+            }
+        }
+    }
+}
--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using Logic;
 
@@ -7,31 +10,111 @@ namespace Tests
 {
     class Program
     {
+        static TextReader Input = Console.In;
+        static TextWriter RegularOutput = Console.Out;
+        static TextWriter AssertOutput = Console.Out;
         static void Main(string[] args)
         {
-            TestAdd(0, 0);
-            TestAdd(0, 1);
-            TestAdd(1, 0);
-            TestAdd(1, 1);
-            Console.WriteLine("Done");
-            Console.ReadLine();
+            TypeInfo typeInfo = typeof(MathClass).GetTypeInfo();
+            MethodInfo originalAddMethodInfo = typeInfo.GetMethod(nameof(MathClass.Add));
+
+            MethodInput[] inputs = new MethodInput[]
+            {
+                    new MethodInput(null, 0m, 0m),
+                    new MethodInput(null, 0m, 1m),
+                    new MethodInput(null, 1m, 0m),
+                    new MethodInput(null, 1m, 1m)
+            };
+
+            if (Mutator.Mutator.MutateSingleFile(@"C:\Users\Nick\source\repos\Seminar\Logic\MathClass.cs",
+                Mutator.Mutator.Mutatations.Add,
+                Mutator.Mutator.Mutatations.Subract)
+                .Any(mutatedAssembly =>
+                {
+                    return inputs.Any(input =>
+                    {
+                        return !TestMutatedMethod(originalAddMethodInfo, mutatedAssembly, input);
+                    });
+                }))
+            {
+                RegularOutput.WriteLine($"Method {originalAddMethodInfo.Name} passed testing");
+            }
+            else
+            {
+                RegularOutput.WriteLine($"Method {originalAddMethodInfo.Name} did not pass testing");
+            }
+            RegularOutput.WriteLine("Done");
+            Input.ReadLine();
         }
 
-        public static void TestAdd(decimal value1, decimal value2)
+        private class MethodInput
         {
-            Assert(MathClass.Add(value1, value2), value1 + value2);
+            public readonly object Instance;
+            public readonly object[] Parameters;
+
+            public MethodInput(object instance, params object[] parametrs)
+            {
+                Instance = instance;
+                Parameters = parametrs;
+            }
         }
 
-        public static void Assert(object object1, object object2,
+        private static bool TestMutatedMethod(MethodInfo originalMethodInfo, Assembly mutatedAssembly, MethodInput methodInput)
+        {
+            TypeInfo mutatedTypeInfo = GetTypeInfoForClass(mutatedAssembly, originalMethodInfo.DeclaringType);
+            MethodInfo mutatedMethodInfo = mutatedTypeInfo.GetMethod(originalMethodInfo.Name);
+            return TestMethod(originalMethodInfo, mutatedMethodInfo, methodInput.Instance, methodInput.Parameters);
+        }
+
+        private static TypeInfo GetTypeInfoForClass(Assembly assembly, Type type)
+        {
+            return assembly.DefinedTypes.Where(t => t.FullName == type.FullName).Single();
+        }
+
+        private static bool TestMethod(MethodInfo original, MethodInfo mutated, object instance, params object[] parameters)
+        {
+            object originalResult = null;
+            Exception originalException = null;
+            try
+            {
+                originalResult = original.Invoke(instance, parameters);
+
+            }
+            catch (Exception e)
+            {
+                originalException = e;
+            }
+
+            object mutatedResult = null;
+            Exception mutatedException = null;
+            try
+            {
+                mutatedResult = mutated.Invoke(instance, parameters);
+            }
+            catch (Exception e)
+            {
+                mutatedException = e;
+            }
+            bool assertObjects = Assert(originalResult, mutatedResult);
+            bool assertExceptions = Assert(mutatedException, originalException);
+            return assertObjects && assertExceptions;
+        }
+
+        public static bool Assert(object object1, object object2,
             [CallerFilePath] string callingFile = null,
             [CallerMemberName] string callingMember = null,
             [CallerLineNumber] int callingLine = 0)
         {
-            if (!Equals(object1, object2))
+            if (Equals(object1, object2))
+            {
+                return true;
+            }
+            else
             {
                 StackTrace st = new StackTrace();
-                Console.WriteLine($"{callingFile}:{callingLine}: {callingMember} failed to assert equals: {object1.ToString()} {object2.ToString()}");
-                Console.WriteLine(st.ToString());
+                AssertOutput.WriteLine($"{callingFile}:{callingLine}: {callingMember} failed to assert equals: {object1?.ToString()} {object2?.ToString()}");
+                AssertOutput.WriteLine(st.ToString());
+                return false;
             }
         }
     }
