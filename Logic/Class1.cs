21ae6e4b0020863c00a7f5384157c67fbfcc500a
Nick Sosinski <Nick.j.sosinski@hotmail.com>
2018-08-25 12:43:12 -0500

    Adding MathClass and Logic

---
+++ Logic/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace Logic
+{
+    public static class MathClass
+    {
+        public static decimal Add(this decimal value1, decimal value2)
+        { return value1 + value2; }
+        public static decimal Subtract(this decimal value1, decimal value2)
+        { return value1 - value2; }
+        public static decimal Multiply(this decimal value1, decimal value2)
+        { return value1 * value2; }
+        public static decimal Divide(this decimal value1, decimal value2)
+        { return value1 / value2; }
+    }
+
+}
